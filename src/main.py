6de5439c9f86c968b3c5960851119d5fcf94350f
poop4ik <vasiltsivandriy@gmail.com>
2023-08-31 16:02:04 +0300

    Setted default main code

--- src/main.py
+++ src/main.py
@@ -1,26 +1,22 @@
-import streamlit as st
-import asyncio
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-
 import scripts.tools.config as config
 import scripts.bot.handlers as handlers
-import scripts.bot.menus as menus
-import scripts.bot.notify as notify
+import scripts.bot.menus    as menus
+import scripts.bot.notify   as notify
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-async def main():
+def main():
     app_builder = ApplicationBuilder()
     app = app_builder.token(config.BOT_TOKEN).build()
 
     if app is not None:
         app.add_handler(MessageHandler(filters.Text(["АДМІНІСТРАТОР", "ВЧИТЕЛЬ"]), handlers.MessagesHandlerAdminTeacher))
+        
         app.add_handler(CommandHandler("start", menus.Start))
-        app.job_queue.run_repeating(notify.CheckAirDangerous, interval=60, first=0)
+        app.job_queue.run_repeating(notify.CheckAirDangerous, interval = 60, first = 0)
         app.add_handler(MessageHandler(filters.Text(), handlers.MessagesHandler))
         app.run_polling()
     else:
         print("Failed to create the application object.")
 
-if __name__ == "main":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    st.write("Hello from Streamlit!")
+if __name__ == "__main__":
+    main()
